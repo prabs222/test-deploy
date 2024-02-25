0c130b37ae0d4d42bfdd81b419721f8d9dc105b2
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2024-02-26 04:03:12 +0530

    test009

--- twamitra/settings.py
+++ twamitra/settings.py
@@ -192,16 +192,20 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # STATICFILES_DIRS = [ BASE_DIR / 'static']
 # STATIC_ROOT =  BASE_DIR / 'static/'
+WHITENOISE_MANIFEST_STRICT = False
 
+STATIC_URL = 'static/'
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = 'static/'
 
 
 
