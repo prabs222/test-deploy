458226cd3e61ecec6924f49e687dfe7a9088449e
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2024-02-26 00:31:53 +0530

    test001

--- twamitra/asgi.py
+++ twamitra/asgi.py
@@ -1,35 +1,3 @@
-# """
-# ASGI config for twamitra project.
-
-# It exposes the ASGI callable as a module-level variable named ``application``.
-
-# For more information on this file, see
-# https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-# """
-
-# import os
-
-# from django.core.asgi import get_asgi_application
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.security.websocket import AllowedHostsOriginValidator
-# from django.core.asgi import get_asgi_application
-
-# from chatApp.routing import websocket_urlpatterns
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twamitra.settings')
-
-
-# application = ProtocolTypeRouter(
-#     {
-#         "http": get_asgi_application(),
-#         "websocket": 
-#             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-#         ,
-#     }
-# )
-
-
 """
 ASGI config for myproject project.
 
@@ -40,35 +8,26 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-import chatApp.routing
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
-
 import django
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twamitra.settings')
-# django.setup()
 
-# application = ProtocolTypeRouter({
-#     'http': get_asgi_application(),
-#     'websocket': AuthMiddlewareStack(
-#         URLRouter(
-#             chat.routing.websocket_urlpatterns
-#         )
-#     )
-# })
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twamitra.settings')
+
+django.setup()
+
+
+django_asgi_app = get_asgi_application()
+
+import chatApp.routing
+
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": 
             AuthMiddlewareStack(URLRouter(chatApp.routing.websocket_urlpatterns))
         ,
